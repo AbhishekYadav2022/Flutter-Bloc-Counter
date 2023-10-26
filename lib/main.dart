a8b7cc90b0c831ea71bf3c638bb70b3523b9d1c3
Abhishek Kumar Yadav <yadavabhishek5869@gmail.com>
2023-10-26 15:39:35 +0530

    Using Multi Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,30 @@
 import 'package:bloc_counter/logic/cubit/counter_cubit.dart';
 import 'package:bloc_counter/presentation/router/app_router.dart';
-import 'package:bloc_counter/presentation/screens/home_screen.dart';
-import 'package:bloc_counter/presentation/screens/second_screen.dart';
-import 'package:bloc_counter/presentation/screens/third_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   final AppRouter _appRouter = AppRouter();
+
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        useMaterial3: true,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          useMaterial3: true,
+        ),
+        onGenerateRoute: _appRouter.onGenerateRoute,
+        debugShowCheckedModeBanner: false,
       ),
-      onGenerateRoute: _appRouter.onGenerateRoute,
-      debugShowCheckedModeBanner: false,
     );
-  }
-
-  @override
-  void dispose() {
-    _appRouter.dispose();
-    super.dispose();
   }
 }
